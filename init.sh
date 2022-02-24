e3f5158af68adf14634ce872c7f869eb470bf0a2
Bruno Souto <bcovies@gmail.com>
2022-02-24 10:58:15 -0300

    Added docker cluster templates

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PROJECTS_TEMPLATES=( "rundeck" "jenkins" "dockermgr" "dockerwkr1" )
+PROJECTS_TEMPLATES=( "rundeck" "jenkins" "dockercluster" )
 CONFD_BIN_FILE=confd-0.16.0-linux-amd64
 PATH_TMP_FILES=/tmp/confd/vagrant
 
